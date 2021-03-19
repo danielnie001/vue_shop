a860a262068ef5477841ead9d164f356671db70f
niehaisheng <haishengnie@163.com>
2021-03-19 15:40:17 +0800

    完成登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar-box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <el-form
+        label-width="0px"
+        class="login_form"
+        :model="loginForm"
+        :rules="loginRules"
+        ref="loginForm"
+      >
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="iconfont icon-yonghu"
+            v-model="loginForm.username"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="iconfont icon-ziyuanxhdpi"
+            v-model="loginForm.password"
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="onSubmit">登录</el-button>
+          <el-button type="info" @click="resetForm('loginForm')"
+            >重置</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 登录表单数据对象
+      loginForm: {
+        username: 'admin',
+        password: 'password123'
+      },
+      loginRules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.loginForm.validate(async valid => {
+        if (!valid) {
+          return
+        }
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) {
+          return this.$message.error('登陆失败！')
+        }
+        this.$message.success('登录成功！')
+        console.log(res)
+        window.sessionStorage.setItem('token', res.data.token)
+        // 登录成功的token，保存到客户端的SessionStorage中
+        // 1.1项目中除了登录之外的API接口，必须要登录之后才能访问
+        // 1.2 token只能在当前网站打开期间有效，所以token保存在sessionStorage中
+        // 2. 通过编程式导航跳转到后台主页，路由地址是/home
+        await this.$router.push('/home')
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #ffffff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar-box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 0 10px;
+  }
+}
+</style>
