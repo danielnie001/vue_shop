977fb9f2e5c3dd6020633104574ba5b700c435f2
niehaisheng <haishengnie@163.com>
2021-03-25 16:33:18 +0800

    完成报表功能开发

---
+++ src/components/report/Report.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <!--面包屑导航区域-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      <div id="main" style="width: 1000px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+// 这里可以导入其他文件(比如：组件，工具js，第三方插件js，json文件，图片文件等等)
+// 例如：import《组件名称》from '《组件路径》';
+import * as echarts from 'echarts'
+import _ from 'lodash'
+export default {
+  // import引入的组件需要注入到对象中才能使用
+  components: {},
+  props: {},
+  data() {
+    // 这里存放数据
+    return {
+      // 需要合并的数据
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  // 计算属性 类似于data概念
+  computed: {},
+  // 监控data中的数据变化
+  watch: {},
+  // 方法集合
+  methods: {},
+  // 生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  // 生命周期 - 挂载完成（可以访问dom元素）
+  async mounted() {
+    var myChart = echarts.init(document.getElementById('main'))
+
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取数据失败')
+    }
+
+    // 指定图表的配置项和数据
+    const result = _.merge(res.data, this.options)
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result)
+  },
+  beforeCreate() {}, // 生命周期 - 创建之前
+  beforeMount() {}, // 生命周期 - 挂载之前
+  beforeUpdate() {}, // 生命周期 - 更新之前
+  updated() {}, // 生命周期 - 更新之后
+  beforeDestroy() {}, // 生命周期 - 销毁之前
+  destroyed() {}, // 生命周期 - 销毁完成
+  activated() {} // 如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+<style lang="less" scoped>
+// @import url();引入公共css类
+</style>
